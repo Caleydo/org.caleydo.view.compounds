584e2ca2b07e8a32cd479a7ec605053d30dcd1eb
Christian Partl <partl@icg.tugraz.at>
2014-03-16 12:37:03 +0100

    getting proper min size of multicompoundslist

--- src/main/java/org/caleydo/view/compounds/ui/MultiCompoundsList.java
+++ src/main/java/org/caleydo/view/compounds/ui/MultiCompoundsList.java
@@ -7,21 +7,15 @@ package org.caleydo.view.compounds.ui;
 
 import gleem.linalg.Vec2f;
 
-import java.awt.Graphics2D;
-import java.awt.Rectangle;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.caleydo.core.data.collection.EDimension;
 import org.caleydo.core.util.collection.Pair.ComparablePair;
-import org.caleydo.core.util.color.Color;
 import org.caleydo.core.util.logging.Logger;
 import org.caleydo.core.view.opengl.layout2.GLElement;
 import org.caleydo.core.view.opengl.layout2.GLElementContainer;
-import org.caleydo.core.view.opengl.layout2.GLGraphics;
 import org.caleydo.core.view.opengl.layout2.GLSandBox;
-import org.caleydo.core.view.opengl.layout2.IGLElementContext;
-import org.caleydo.core.view.opengl.layout2.GLElement.EVisibility;
 import org.caleydo.core.view.opengl.layout2.basic.ScrollBar;
 import org.caleydo.core.view.opengl.layout2.basic.ScrollingDecorator;
 import org.caleydo.core.view.opengl.layout2.basic.ScrollingDecorator.IHasMinSize;
@@ -29,43 +23,26 @@ import org.caleydo.core.view.opengl.layout2.geom.Rect;
 import org.caleydo.core.view.opengl.layout2.layout.GLLayouts;
 import org.caleydo.core.view.opengl.layout2.layout.GLMinSizeProviders;
 import org.caleydo.core.view.opengl.layout2.layout.GLPadding;
-import org.caleydo.core.view.opengl.layout2.layout.GLSizeRestrictiveFlowLayout;
 import org.caleydo.core.view.opengl.layout2.layout.GLSizeRestrictiveFlowLayout2;
-import org.openscience.cdk.DefaultChemObjectBuilder;
-import org.openscience.cdk.exception.InvalidSmilesException;
-import org.openscience.cdk.graph.ConnectivityChecker;
-import org.openscience.cdk.interfaces.IAtomContainer;
-import org.openscience.cdk.interfaces.IAtomContainerSet;
-import org.openscience.cdk.layout.StructureDiagramGenerator;
-import org.openscience.cdk.renderer.AtomContainerRenderer;
-import org.openscience.cdk.renderer.font.AWTFontManager;
-import org.openscience.cdk.renderer.generators.BasicAtomGenerator;
-import org.openscience.cdk.renderer.generators.BasicBondGenerator;
-import org.openscience.cdk.renderer.generators.BasicSceneGenerator;
-import org.openscience.cdk.renderer.generators.IGenerator;
-import org.openscience.cdk.renderer.visitor.AWTDrawVisitor;
-import org.openscience.cdk.smiles.SmilesParser;
-
-import com.jogamp.opengl.util.awt.TextureRenderer;
 
 /**
  * element of this view
- * 
+ *
  * @author Hendrik Strobelt
- * 
+ *
  */
 public class MultiCompoundsList extends GLElementContainer implements IHasMinSize {
 	private static final Logger log = Logger.create(MultiCompoundsList.class);
 
-	
+
 	protected ScrollingDecorator scrollingDecorator;
-	
-	protected ScrollableCompoundsList body;  
+
+	protected ScrollableCompoundsList body;
 
 //	 public MultiCompoundsElement(){
-//		 
+//
 //	 }
-	
+
 	protected class ScrollableCompoundsList extends GLElementContainer{
 		private  List<ComparablePair<String, String>> smilesList;
 
@@ -75,28 +52,28 @@ public class MultiCompoundsList extends GLElementContainer implements IHasMinSiz
 			setLayout(new GLSizeRestrictiveFlowLayout2(false, 5, GLPadding.ZERO));
 			setMinSizeProvider(GLMinSizeProviders.createVerticalFlowMinSizeProvider(this,
 					5, GLPadding.ZERO));
-			
+
 			for (ComparablePair<String, String> smile : this.smilesList) {
 //				add(new CompoundsElement(smile,400,400));
 				add(new CompoundsElement(smile.getSecond(), smile.getFirst()));
 //				add(new CompoundsElement(smile.getSecond(), smile.getFirst(),300,300));
 			}
-			
+
 		}
-		
-		
-		
+
+
+
 		@Override
 		public void layout(int deltaTimeMs) {
 			super.layout(deltaTimeMs);
 
 			Rect clippingArea = scrollingDecorator.getClipingArea();
-			
+
 			for (GLElement child : this) {
 				Rect bounds = child.getRectBounds();
 				if (child.getVisibility() != EVisibility.NONE) {
 					if (clippingArea.asRectangle2D().intersects(bounds.asRectangle2D())) {
-						
+
 						if (child.getVisibility() != EVisibility.PICKABLE) {
 							child.setVisibility(EVisibility.PICKABLE);
 							repaintAll();
@@ -118,22 +95,23 @@ public class MultiCompoundsList extends GLElementContainer implements IHasMinSiz
 			// minSize.setX(getSize().x());
 			return super.getMinSize();
 		}
-		
-		
+
+
 	}
-	
-	
-	
+
+
+
 	//List<ComparablePair<String, String>> smiles
 	public MultiCompoundsList(final List<ComparablePair<String, String>> smilesList) {
 		super();
 //		setLayout(new GLSizeRestrictiveFlowLayout2(false, 1, GLPadding.ZERO));
 		setLayout(GLLayouts.LAYERS);
-		
+		setMinSizeProvider(GLMinSizeProviders.createLayeredMinSizeProvider(this));
+
 		body = new ScrollableCompoundsList(smilesList);
-		
-		
-		scrollingDecorator = new ScrollingDecorator(body, new ScrollBar(true), 
+
+
+		scrollingDecorator = new ScrollingDecorator(body, new ScrollBar(true),
 				new ScrollBar(false), 8, EDimension.RECORD );
 		scrollingDecorator.setMinSizeProvider(
 //				new IHasMinSize() {
@@ -144,23 +122,23 @@ public class MultiCompoundsList extends GLElementContainer implements IHasMinSiz
 //			}
 //		}
 		body		);
-		
+
 		add(scrollingDecorator);
 		relayout();
-		
+
 	}
-	
+
 	@Override
 	public Vec2f getMinSize() {
-		return new Vec2f(300,300);
+		return super.getMinSize();
 	}
-	
+
 
 	public GLElement asGLElement() {
 		return scrollingDecorator;
 	}
 
-	
+
 	public static void main(String[] args) {
 		// the tough one:
 		ArrayList<ComparablePair<String, String> > list = new ArrayList<ComparablePair<String, String> >();
@@ -170,7 +148,7 @@ public class MultiCompoundsList extends GLElementContainer implements IHasMinSiz
 		list.add(new ComparablePair<String, String>("abc","COC1=C(C(=CC=C1)OC)OCCNCC2COC3=CC=CC=C3O2.Cl"));
 		list.add(new ComparablePair<String, String>("abc","COC1=C(C(=CC=C1)OC)OCCNCC2COC3=CC=CC=C3O2.Cl"));
 		list.add(new ComparablePair<String, String>("abc","COC1=C(C(=CC=C1)OC)OCCNCC2COC3=CC=CC=C3O2.Cl"));
-		
+
 		GLSandBox.main(args, new MultiCompoundsList(list));
 
 		// GLSandBox.main(args, new
